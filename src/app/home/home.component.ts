5eb370e8da57265b20b266a0e590d909c88ce0b7
Frank Velaio <fmurillo@velaio.com>
2019-11-04 22:48:15 -0500

    Template To Preview

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,31 +9,20 @@ import { HttpClient } from '@angular/common/http';
 export class HomeComponent implements OnInit {
 
   list_array: any;
-  items: Array<any>;
 
   constructor(private http: HttpClient) {
 
   }
 
   ngOnInit() {
-    /*try {
-      this.http.get('http://apiservicepruebas.portalclientes.finesa.com.co/api/v1/aseguradoras').subscribe((response) => {
-        this.list_array = response;
-        this.items = this.list_array.data.aseguradoras;
-        console.log(this.items);
-      });
-    }catch (err) {
-      console.log('Lo sentimos hay problemas de conexión', err);
-    }*/
     try {
       this.http.get('http://jsonplaceholder.typicode.com/users').subscribe((response) => {
         this.list_array = response;
-        console.log(this.list_array);
+        console.log('Users Api ',this.list_array);
       });
     }catch (err) {
       console.log('Lo sentimos hay problemas de conexión', err);
     }
-
   }
 
 }
