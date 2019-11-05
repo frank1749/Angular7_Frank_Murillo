f7b81962fe6eb831d950e0b72521d7f9593cbc40
Frank Velaio <fmurillo@velaio.com>
2019-11-04 20:21:56 -0500

    Init Project

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-raiz',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Lucasian';
+}
